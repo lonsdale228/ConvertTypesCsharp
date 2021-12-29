63ffca4afddbfc34737b0b3a1888848f3d6fcc54
lonsdale228 <gerashchenkovladislav@onua.edu.ua>
2021-12-29 17:27:32 +0200

    Добавьте файлы проекта.

---
+++ ConvertTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConvertTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (int.TryParse("123", out int result)) //если получиться преобразовать переменную, тогда выполняется код
+            {
+                Console.WriteLine(result);          //типа try except
+            }
+
+            int i = int.Parse("42");
+
+            string s = "str " + i;
+
+
+            Console.WriteLine(i);
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
